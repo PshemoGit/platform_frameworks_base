f1837c5df438317ec14cbe43e4662dfd26782776
SKULSHADY <anushekprasal@gmail.com>
2019-06-08 14:12:24 +0700

    base: Make FP detection in pocket mode configurable [1/2]
    Signed-off-by: NurKeinNeid <mralexman3000@gmail.com>

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -4989,6 +4989,11 @@ public final class Settings {
          * @hide
          */
         public static final String POCKET_JUDGE = "pocket_judge";
+        
+        /**
+         * @hide
+         */
+        public static final String POCKET_JUDGE_ALLOW_FP = "pocket_judge_allow_fp";
 
         /**
          * @hide
